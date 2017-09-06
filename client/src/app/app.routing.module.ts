abb0584003ed8af7f9ad42b119bc6b5a81665a88
Tianhao Qiu <lqiu1127@yahoo.com>
2017-09-06 05:42:20 -0700

    register completely works, user registers to DB

--- client/src/app/app.routing.module.ts
+++ client/src/app/app.routing.module.ts
@@ -2,27 +2,31 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
 
+// Array of Angular Routes
 const appRoutes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    },    
-    {
-        path: 'dashboard',
-        component: DashboardComponent
-    },
-    { 
-        path: '**',
-        component: HomeComponent 
-    }
+  {
+    path: '',
+    component: HomeComponent // The Default Route
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent // The Dashboard Route
+  },
+  {
+    path: 'register',
+    component: RegisterComponent // The Register Route
+  },
+  { path: '**', component: HomeComponent } // The "Catch-All" Route
 ];
 
 @NgModule({
-    declarations: [],
-    imports: [RouterModule.forRoot(appRoutes)],
-    providers: [],
-    bootstrap: [],
-    exports: [RouterModule]
+  declarations: [],
+  imports: [RouterModule.forRoot(appRoutes)],
+  providers: [],
+  bootstrap: [],
+  exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
---
+++ client/src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AuthService {
+
+  domain = "http://localhost:3000"
+
+  constructor(
+    private http: Http
+  ) { }
+
+  // Function to check if username is taken
+  checkUsername(username) {
+    return this.http.get(this.domain + '/authentication/checkUsername/' + username).map(res => res.json());
+  }
+
+  // Function to check if e-mail is taken
+  checkEmail(email) {
+    return this.http.get(this.domain + '/authentication/checkEmail/' + email).map(res => res.json());
+  }
+
+  registerUser(user) {
+    return this.http.post(this.domain + '/authentication/register', user).map(res => res.json());
+  }
+}
